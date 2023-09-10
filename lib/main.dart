87787d0406cc927d86d08d7149fd4632173d17a3
abdulmalek9 <akabdalmalek@gmail.com>
2023-09-10 16:17:00 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/views/home_view.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NewsHomeView(),
+    );
+  }
+}
