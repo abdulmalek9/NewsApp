37553b3ecd04cb87e41c48473c9d4e2a7d188e16
abdulmalek9 <akabdalmalek@gmail.com>
2023-09-14 23:26:24 +0300

    add news service and news item model to get news from api

---
+++ lib/models/news_item_model.dart
@@ -0,0 +1,7 @@
+class NewsItemModel {
+  final String? image;
+  final String title;
+  final String? description;
+  NewsItemModel(
+      {required this.image, required this.title, required this.description});
+}
---
+++ lib/services/news_service.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:news_app/models/news_item_model.dart';
+
+class NewsService {
+  final Dio dio;
+
+  final apiKey = "402e7b1e18b14ca8adf16af66a7c5317";
+
+  NewsService(this.dio);
+
+  getnews() async {
+    Response response = await dio.get(
+        "https://newsapi.org/v2/top-headlines?country=eg&category=general&apikey=$apiKey");
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData['articles'];
+
+    List<NewsItemModel> articleList = [];
+    for (var article in articles) {
+      NewsItemModel newsItemModel = NewsItemModel(
+          image: article['urlToImage'],
+          title: article['title'],
+          description: article['description']);
+      articleList.add(newsItemModel);
+    }
+  }
+}
